7f0335261d890ce635073968f64b9fe91a89c6de
Jhonatan Chaparro <167357134+Jhoanatanchaparro@users.noreply.github.com>
2024-10-11 14:12:29 -0500

    Add files via upload

---
+++ Variables/Ejercicio1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Variables
+{
+    internal class Ejercicio1
+    {
+        // Método Run que será llamado desde la clase Program
+        public void Run()
+        {
+            Console.WriteLine("Ingrese el primer numero: ");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Ingrese el segundo numero: ");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Ingrese el tercer numero: ");
+            double num3 = Convert.ToDouble(Console.ReadLine());
+
+            double resultado = num1 * num2 * num3;
+            Console.WriteLine($"El resultado de la multiplicacion es : {resultado}");
+        }
+    }
+}
+
---
+++ Variables/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Variables
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //run Ejercicio 1
+            Ejercicio1 ejercicio1 = new Ejercicio1();
+            ejercicio1.Run();
+
+            // run Ejercicio 2 
+            Ejercicio2 ejercicio2 = new Ejercicio2();
+            ejercicio2.Run();
+
+            // run Ejercicio 3 
+            Ejercicio_3 ejercicio_3 = new Ejercicio_3();
+            ejercicio_3.Run();
+            
+
+        }
+    }
+}
